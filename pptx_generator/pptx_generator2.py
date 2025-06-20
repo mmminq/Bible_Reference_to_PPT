9688d73cbc5db85261efe8a526f8da271606174e
Minkyu Kim <minkyu7287@gmail.com>
2025-06-20 10:56:20 +0900

    pptx 서식 선택 가능

--- pptx_generator/pptx_generator2.py
+++ pptx_generator/pptx_generator2.py
@@ -14,7 +14,15 @@ ESV_BIBLE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '
 TEMPLATE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/pptx_template/template.pptx'
 OUTPUT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/pptx_template/output.pptx'
 
-bible_books = [ ... ]  # 동일하게 유지됨
+bible_books = [
+    "창세기", "출애굽기", "레위기", "민수기", "신명기", "여호수아", "사사기", "룻기", "사무엘상", "사무엘하",
+    "열왕기상", "열왕기하", "역대상", "역대하", "에스라", "느헤미야", "에스더", "욥기", "시편", "잠언",
+    "전도서", "아가", "이사야", "예레미야", "예레미야애가", "에스겔", "다니엘", "호세아", "요엘", "아모스",
+    "오바댜", "요나", "미가", "나훔", "하박국", "스바냐", "학개", "스가랴", "말라기", "마태복음", "마가복음",
+    "누가복음", "요한복음", "사도행전", "로마서", "고린도전서", "고린도후서", "갈라디아서", "에베소서", "빌립보서", "골로새서",
+    "데살로니가전서", "데살로니가후서", "디모데전서", "디모데후서", "디도서", "빌레몬서", "히브리서", "야고보서", "베드로전서", "베드로후서",
+    "요한일서", "요한이서", "요한삼서", "유다서", "요한계시록"
+]
 
 DEFAULT_STYLE = {
     "kor_title": {"font": "나눔스퀘어 네오 ExtraBold", "size": 37.3, "color": "#1F3337"},
@@ -29,11 +37,12 @@ def hex_to_rgb(hex_color):
 
 
 def duplicate_slide_with_blank_layout(prs, slide):
-        new_slide = prs.slides.add_slide(blank_layout)  # 빈 레이아웃 사용
-        for shape in slide.shapes:
-            new_shape = copy.deepcopy(shape.element)
-            new_slide.shapes._spTree.insert_element_before(new_shape, 'p:extLst')
-        return new_slide
+    blank_layout = prs.slide_layouts[6]
+    new_slide = prs.slides.add_slide(blank_layout)  # 빈 레이아웃 사용
+    for shape in slide.shapes:
+        new_shape = copy.deepcopy(shape.element)
+        new_slide.shapes._spTree.insert_element_before(new_shape, 'p:extLst')
+    return new_slide
 
 def add_scripture_to_ppt(template_path, verse_texts, verse_texts_eng, style, output_path="output.pptx"):
     if not os.path.exists(resource_path(template_path)):
@@ -41,60 +50,143 @@ def add_scripture_to_ppt(template_path, verse_texts, verse_texts_eng, style, out
     
     prs = Presentation(template_path)
     
-    # 빈 레이아웃 (일반적으로 '빈 화면'은 레이아웃 인덱스 6번입니다)
-    blank_layout = prs.slide_layouts[6]
-
-    def duplicate_slide_with_blank_layout(prs, slide):
-        new_slide = prs.slides.add_slide(blank_layout)  # 빈 레이아웃 사용
-        for shape in slide.shapes:
-            new_shape = copy.deepcopy(shape.element)
-            new_slide.shapes._spTree.insert_element_before(new_shape, 'p:extLst')
-        return new_slide
-    
     # 슬라이드 수 부족하면 복제
     while len(prs.slides) < len(verse_texts):
         duplicate_slide_with_blank_layout(prs, prs.slides[-1])
+        
+    for idx, (address, verse) in enumerate(verse_texts):
+        slide = prs.slides[idx]
+        
+        # 2번째 텍스트 상자 (인덱스 1)에 개역개정 주소 텍스트 추가
+        text_shape = slide.shapes[1]
+        text_frame = text_shape.text_frame # type: ignore
+        text_frame.clear()
+        for i, line in enumerate(address.split('\n')):
+            if len(address.split('\n')) > 2:
+                if i == 0:
+                    p = text_frame.paragraphs[0]
+                else:
+                    p = text_frame.add_paragraph()
+                p.text = line
+                p.font.color.rgb = hex_to_rgb(style['kor_title']['color'])
+                p.font.size = Pt(style['kor_title']['size'])
+                p.font.name = style['kor_title']['font']
+            else:
+                words = line.split()
+                if not words:
+                    continue
+                for j, word in enumerate(words):
+                    if i == 0 and j == 0:
+                        p = text_frame.paragraphs[0]
+                    else:
+                        p = text_frame.add_paragraph()
+                    p.text = word
+                    p.font.color.rgb = hex_to_rgb(style['kor_body']['color'])
+                    p.font.size = Pt(style['kor_body']['size'])
+                    p.font.name = style['kor_body']['font']
 
-    for i, (kor, eng) in enumerate(zip(verse_texts, verse_texts_eng)):
-        slide = prs.slides[i]
+        # 7번째 텍스트 상자 (인덱스 6)에 개역개정 본문 텍스트 추가
+        text_shape = slide.shapes[6]
+        text_frame = text_shape.text_frame # type: ignore
+        text_frame.clear()
+        for i, line in enumerate(verse.split('\n')):
+            if i == 0:
+                p = text_frame.paragraphs[0]
+            else:
+                p = text_frame.add_paragraph()
+            superscript_map = str.maketrans("0123456789:", "⁰¹²³⁴⁵⁶⁷⁸⁹˸")
+            if len(address.split('\n')) > 2:
+                sup = str(address.split('\n')[i].split(' ')[1]).translate(superscript_map)
+            else:
+                sup = str(line.split(' ')[0]).translate(superscript_map)
+            p.text = sup+' '+' '.join(line.split(' ')[1:])
+            p.font.color.rgb = hex_to_rgb(style['kor_body']['color'])
+            p.font.size = Pt(style['kor_body']['size'])
+            p.font.name = style['kor_body']['font']
 
-        kor_title_shape = slide.shapes[1]
-        kor_content_shape = slide.shapes[6]
-        eng_title_shape = slide.shapes[5]
-        eng_content_shape = slide.shapes[7]
+        prs.save(output_path)
 
-        # Title (Korean)
-        run = kor_title_shape.text_frame.clear().add_paragraph().add_run()
-        run.text = kor['title']
-        run.font.name = style['kor_title']['font']
-        run.font.size = Pt(style['kor_title']['size'])
-        run.font.color.rgb = hex_to_rgb(style['kor_title']['color'])
+    for idx, (address, verse) in enumerate(verse_texts_eng):
+        slide = prs.slides[idx]
 
-        # Body (Korean + English)
-        kor_content_shape.text_frame.clear()
+        # 6번째 텍스트 상자 (인덱스 5)에 ESV 주소 텍스트 추가
+        text_shape = slide.shapes[5]
+        text_frame = text_shape.text_frame # type: ignore
+        text_frame.clear()
+        for i, line in enumerate(address.split('\n')):
+            if i == 0:
+                p = text_frame.paragraphs[0]
+            else:
+                p = text_frame.add_paragraph()
+            p.text = line
+            p.font.color.rgb = hex_to_rgb(style['eng_title']['color'])
+            p.font.size = Pt(style['eng_title']['size'])
+            p.font.name = style['eng_title']['font']
 
-        p_kor = kor_content_shape.text_frame.paragraphs[0]
-        run_kor = p_kor.add_run()
-        run_kor.text = kor['body']
-        run_kor.font.name = style['kor_body']['font']
-        run_kor.font.size = Pt(style['kor_body']['size'])
-        run_kor.font.color.rgb = hex_to_rgb(style['kor_body']['color'])
 
-        p_eng = eng_content_shape.text_frame.add_paragraph()
-        run_eng_title = p_eng.add_run()
-        run_eng_title.text = eng['title'] + "\n"
-        run_eng_title.font.name = style['eng_title']['font']
-        run_eng_title.font.size = Pt(style['eng_title']['size'])
-        run_eng_title.font.color.rgb = hex_to_rgb(style['eng_title']['color'])
+        # 8번째 텍스트 상자 (인덱스 7)에 ESV 본문 텍스트 추가
+        text_shape = slide.shapes[7]
+        text_frame = text_shape.text_frame # type: ignore
+        text_frame.clear()
+        # text_frame.text = verse
+        for i, line in enumerate(verse.split('\n')):
+            if i == 0:
+                p = text_frame.paragraphs[0]
+            else:
+                p = text_frame.add_paragraph()
+            superscript_map = str.maketrans("0123456789:", "⁰¹²³⁴⁵⁶⁷⁸⁹˸")
+            sup = str(line.split(' ')[0]).translate(superscript_map)
+            if len(address.split('\n')) > 2:
+                sup = str(address.split('\n')[i].split(' ')[1]).translate(superscript_map)
+            else:
+                sup = str(line.split(' ')[0]).translate(superscript_map)
+            p.text = sup+' '+' '.join(line.split(' ')[1:])
+            p.font.color.rgb = hex_to_rgb(style['eng_body']['color'])
+            p.font.size = Pt(style['eng_body']['size'])
+            p.font.name = style['eng_body']['font']
 
-        p_eng_body = eng_content_shape.text_frame.add_paragraph()
-        run_eng_body = p_eng_body.add_run()
-        run_eng_body.text = eng['body']
-        run_eng_body.font.name = style['eng_body']['font']
-        run_eng_body.font.size = Pt(style['eng_body']['size'])
-        run_eng_body.font.color.rgb = hex_to_rgb(style['eng_body']['color'])
+        prs.save(output_path)
 
-    prs.save(output_path)
+    # for i, (kor, eng) in enumerate(zip(verse_texts, verse_texts_eng)):
+    #     slide = prs.slides[i]
+
+    #     kor_title_shape = slide.shapes[1]
+    #     kor_content_shape = slide.shapes[6]
+    #     eng_title_shape = slide.shapes[5]
+    #     eng_content_shape = slide.shapes[7]
+
+    #     # Title (Korean)
+    #     run = kor_title_shape.text_frame.clear().add_paragraph().add_run()
+    #     run.text = kor['title']
+    #     run.font.name = style['kor_title']['font']
+    #     run.font.size = Pt(style['kor_title']['size'])
+    #     run.font.color.rgb = hex_to_rgb(style['kor_title']['color'])
+
+    #     # Body (Korean + English)
+    #     kor_content_shape.text_frame.clear()
+
+    #     p_kor = kor_content_shape.text_frame.paragraphs[0]
+    #     run_kor = p_kor.add_run()
+    #     run_kor.text = kor['body']
+    #     run_kor.font.name = style['kor_body']['font']
+    #     run_kor.font.size = Pt(style['kor_body']['size'])
+    #     run_kor.font.color.rgb = hex_to_rgb(style['kor_body']['color'])
+
+    #     p_eng = eng_content_shape.text_frame.add_paragraph()
+    #     run_eng_title = p_eng.add_run()
+    #     run_eng_title.text = eng['title'] + "\n"
+    #     run_eng_title.font.name = style['eng_title']['font']
+    #     run_eng_title.font.size = Pt(style['eng_title']['size'])
+    #     run_eng_title.font.color.rgb = hex_to_rgb(style['eng_title']['color'])
+
+    #     p_eng_body = eng_content_shape.text_frame.add_paragraph()
+    #     run_eng_body = p_eng_body.add_run()
+    #     run_eng_body.text = eng['body']
+    #     run_eng_body.font.name = style['eng_body']['font']
+    #     run_eng_body.font.size = Pt(style['eng_body']['size'])
+    #     run_eng_body.font.color.rgb = hex_to_rgb(style['eng_body']['color'])
+
+    # prs.save(output_path)
 
 def collect_style():
     return {
